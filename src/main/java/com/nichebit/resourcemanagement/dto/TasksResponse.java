702c35f6ffaf5d1551087ec2102804456f7a97b0
deepakgullapalli <deepakgullapalli9701@gmail.com>
2023-08-07 15:18:32 +0530

    same

---
+++ src/main/java/com/nichebit/resourcemanagement/dto/TasksResponse.java
@@ -0,0 +1,16 @@
+package com.nichebit.resourcemanagement.dto;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TasksResponse {
+
+	
+	private String task;
+}
