56d790b75a4017d2a1c9ed60ec22967f86d36873
deepakgullapalli <deepakgullapalli9701@gmail.com>
2023-08-07 14:49:03 +0530

    dtoclass

---
+++ src/main/java/com/nichebit/resourcemanagement/dto/dtoForProjectResponse.java
@@ -0,0 +1,17 @@
+package com.nichebit.resourcemanagement.dto;
+
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class dtoForProjectResponse {
+
+	
+	private String projectname;
+	private long id;
+}
